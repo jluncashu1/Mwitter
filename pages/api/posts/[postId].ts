2765f930b5078986aa88e98e3783894189ad7bbe
jluncashu1 <167792468+jluncashu1@users.noreply.github.com>
2024-04-23 08:09:22 +0300

    Update [postId].ts

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -38,3 +38,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).end();
     }
 }
+
+export const config = {
+    api: {
+        responseLimit: false,
+    },
+};
